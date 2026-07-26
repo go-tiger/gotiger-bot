c6bf3f69e589a55c7b3f103921a40e4b66d55acf
go-tiger <dev.gotiger@gmail.com>
2026-07-26 17:36:31 +0900

    chore: TypeORM 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync(typeOrmConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
