e98046ece3bc8d151065d06b08c901167da3261b
go-tiger <dev.gotiger@gmail.com>
2026-07-27 17:47:31 +0900

    feat: Discord 봇 연결 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { NecordModule } from 'necord';
 import { typeOrmConfig } from './common/config/typeorm.config';
+import { discordConfig } from './common/config/discord.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -9,6 +11,7 @@ import { AppService } from './app.service';
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync(typeOrmConfig),
+    NecordModule.forRootAsync(discordConfig),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,19 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  app.enableShutdownHooks();
+
+  process.on('SIGINT', async () => {
+    await app.close();
+    process.exit(0);
+  });
+
+  process.on('SIGTERM', async () => {
+    await app.close();
+    process.exit(0);
+  });
+
   await app.listen(configService.get<number>('PORT') ?? 3000);
 }
 bootstrap();
