ece28ebab31863bab0dc5cca18f66ede06fe9b1a
go-tiger <dev.gotiger@gmail.com>
2026-07-27 15:27:24 +0900

    fix: 엔티티 자동 로드되도록 entities 경로 추가

--- src/common/config/typeorm.config.ts
+++ src/common/config/typeorm.config.ts
@@ -10,7 +10,7 @@ export const typeOrmConfig: TypeOrmModuleAsyncOptions = {
     username: configService.get<string>('DB_USERNAME'),
     password: configService.get<string>('DB_PASSWORD'),
     database: configService.get<string>('DB_DATABASE'),
-    autoLoadEntities: true,
+    entities: [__dirname + '/../entities/**/*.entity{.ts,.js}'],
     namingStrategy: new SnakeNamingStrategy(),
     synchronize: true,
   }),
