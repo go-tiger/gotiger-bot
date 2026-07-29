e9c466ce0014bdc187f1297a23ba7bd87af6694f
go-tiger <dev.gotiger@gmail.com>
2026-07-29 20:17:30 +0900

    chore: src 초기화 및 재설계 준비

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { EventEmitterModule } from '@nestjs/event-emitter';
-import { ScheduleModule } from '@nestjs/schedule';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { NecordModule } from 'necord';
-import { typeOrmConfig } from './common/config/typeorm.config';
-import { discordConfig } from './common/config/discord.config';
-import { AuthModule } from './modules/auth/auth.module';
-import { DiscordModule } from './discord/discord.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    EventEmitterModule.forRoot(),
-    ScheduleModule.forRoot(),
-    TypeOrmModule.forRootAsync(typeOrmConfig),
-    NecordModule.forRootAsync(discordConfig),
-    AuthModule,
-    DiscordModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-
-  app.enableShutdownHooks();
-
-  process.on('SIGINT', async () => {
-    await app.close();
-    process.exit(0);
-  });
-
-  process.on('SIGTERM', async () => {
-    await app.close();
-    process.exit(0);
-  });
-
-  await app.listen(configService.get<number>('PORT') ?? 3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
